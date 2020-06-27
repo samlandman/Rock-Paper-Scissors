f9d669c1b749af5c1769ea656ab13c72110e9da7
sam landman <sam landman>
2020-06-27 13:38:53 +0100

    Basic Frameworking done

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 require_relative "person"
 
 class Game
-  attr_accessor :array
-  def initialize
+  attr_accessor :array, :player_1, :player_2
+  def initialize(name = "default", name2 = nil)
     @array = ["Rock","Paper","Scissors"]
-    @player_1 = 0
-    @computer = 0
+    @player_1 = Person.new(name)
+    @player_2 = Person.new(name2)
   end
 
   def play(choice)
@@ -19,16 +19,28 @@ class Game
     elsif
       @array.last == choice_1 && @array[0] == computer_choice
       string << "#{computer_choice} beats #{choice_1}. Computer wins"
+      @player_2.points += 1
     elsif
       @array.last == computer_choice && @array[0] == choice_1
       string << "#{choice_1} beats #{computer_choice}. Player wins"
+      @player_1.points += 1
     elsif
       @array.index(choice_1) >= @array.index(computer_choice)
       string << "#{choice_1} beats #{computer_choice}. Player wins"
+      @player_1.points += 1
     elsif
       @array.index(choice_1) <= @array.index(computer_choice)
       string << "#{computer_choice} beats #{choice_1}. Computer wins"
+      @player_2.points += 1
     end
     string
+  end
+
+  def winner
+    if @player_2.points > @player_1.points
+      "#{@player_2.name} wins!"
+    else
+      "#{@player_1.name} wins!"
+    end
   end
 end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,6 @@
+def sign_in_and_play
+  visit('/')
+    fill_in :name, with: "Sam"
+    fill_in :RPS_input, with "Rock"
+    click_button 'Submit'
+end
