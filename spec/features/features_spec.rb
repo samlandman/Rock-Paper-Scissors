aa5ec11cc19fa696f86fd07757cd271a2c0def98
sam landman <sam landman>
2020-06-29 09:07:09 +0100

    Player enters Rock and Computer Chooses Rock. Round Drawn

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,14 +1,16 @@
 feature 'Enter names' do
   scenario "player enters their name and it returns input on the screen" do
-    visit '/'
-    fill_in :name, with: 'Sam'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sam'
   end
   scenario "player enters their choice and it returns input on the screen" do
-    visit '/'
-    fill_in :name, with: 'Sam'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Rock'
   end
+  scenario "player enters Rock and Computer Chooses Rock. Round Drawn" do
+    srand(0)
+    sign_in_and_play
+    expect(page).to have_content 'draw'
+  end
+
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,5 @@
 def sign_in_and_play
   visit('/')
-    fill_in :name, with: "Sam"
-    #fill_in :RPS_input, with "Rock"
+    fill_in :name, with: 'Sam'
     click_button 'Submit'
 end
