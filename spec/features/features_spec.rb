853b88833e2dc5a102dab4ea3f6b7488b7d19c9c
sam landman <sam landman>
2020-06-29 09:03:49 +0100

    player enters their choice and it returns input on the screen

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -5,4 +5,10 @@ feature 'Enter names' do
     click_button 'Submit'
     expect(page).to have_content 'Sam'
   end
+  scenario "player enters their choice and it returns input on the screen" do
+    visit '/'
+    fill_in :name, with: 'Sam'
+    click_button 'Submit'
+    expect(page).to have_content 'Rock'
+  end
 end
