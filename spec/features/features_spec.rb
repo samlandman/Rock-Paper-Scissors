13040abe658395f2d4448da8e090d74d808ac864
sam landman <sam landman>
2020-06-29 08:58:09 +0100

    Capybara testing improved

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,12 +1,8 @@
-# feature 'test homepage displays "Testing infrastructure working!"' do
-#   scenario "arriving on homepage" do
-#     visit '/'
-#     expect(page).to have_content('Testing infrastructure working!')
-#   end
-# end
 feature 'Enter names' do
-  scenario 'players enter name and it returns input on the screen' do
-    sign_in_and_play
+  scenario "player enters their name and it returns input on the screen" do
+    visit '/'
+    fill_in :name, with: 'Sam'
+    click_button 'Submit'
     expect(page).to have_content 'Sam'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require './app.rb'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'features/web_helpers.rb'
+Capybara.app = RPS
+RACK_ENV='test'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
